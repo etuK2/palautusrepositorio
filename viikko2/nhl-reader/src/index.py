90b89d1e66053f72596a79c754161524f9af1bde
EetuKilpi <eetu.kilpi@outlook.com>
2024-11-04 19:03:32 +0200

    viikko2

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,15 +1,45 @@
 from player_reader import PlayerReader
 from player_stats import PlayerStats
+from rich.console import Console
+from rich.table import Table
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    url = "https://studies.cs.helsinki.fi/nhlstats"
     reader = PlayerReader(url)
-    stats = PlayerStats(reader)
-    players = stats.top_scorers_by_nationality("FIN")
 
-    print("Players from FIN:\n")
-    for player in players:
-        print(player)
+    available_seasons = [
+        "2018-19", "2019-20", "2020-21", 
+        "2021-22", "2022-23", "2023-24", 
+        "2024-25"
+    ]
+    console = Console()
+    
+    print("NHL statistics by nationality\n")
+
+    season = input(f"Select season [2018-19/2019-20/2020-21/2021-22/2022-23/2023-24/2024-25/]: ")
+
+    reader.get_players(season)
+    stats = PlayerStats(reader)
+
+    while True:
+        nationality = input("\nSelect nationality [AUT/CZE/AUS/SWE/GER/DEN/SUI/SVK/NOR/RUS/CAN/LAT/BLR/SLO/USA/FIN/GBR/]: ").strip()
+
+        players = stats.top_scorers_by_nationality(nationality)
+
+        table = Table(title=f"Top scorers of {nationality} season {season}")
+
+        table.add_column("Name", style="cyan", no_wrap=True)
+        table.add_column("Team", style="purple")
+        table.add_column("Goals", style="green")
+        table.add_column("Assists", style="green")
+        table.add_column("Points", style="green")
+
+        if players:
+            for player in players:
+                table.add_row(player.name, player.team, str(player.goals), str(player.assists), str(player.points))
+            console.print(table)
+        else:
+            break
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player_reader.py
+++ viikko2/nhl-reader/src/player_reader.py
@@ -4,8 +4,10 @@ from player import Player
 class PlayerReader:
     def __init__(self, url):
         self.url = url
-        self.players = self.get_players()
+        self.players = []
 
-    def get_players(self):
-        response = requests.get(self.url).json()
-        return [Player(player_dict) for player_dict in response]
+    def get_players(self, season):
+        response = requests.get(f"{self.url}/{season}/players").json()
+        self.players = [Player(player_dict) for player_dict in response]
+        return self.players
+
