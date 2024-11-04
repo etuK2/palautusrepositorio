beddaa6eb1b0f5a7471903b69ef3302e7a6fd24c
EetuKilpi <eetu.kilpi@outlook.com>
2024-11-04 18:22:11 +0200

    viikko2

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,9 +1,14 @@
 class Player:
-    def __init__(self, dict):
-        self.name = dict['name']
-        self.team = dict['team']
-        self.goals = dict['goals']
-        self.assists = dict['assists']
+    def __init__(self, player_dict):
+        self.name = player_dict['name']
+        self.team = player_dict['team']
+        self.goals = player_dict['goals']
+        self.assists = player_dict['assists']
+
+    @property
+    def points(self):
+        return self.goals + self.assists
 
     def __str__(self):
-        return f"{self.name} team {self.team}  goals {self.goals} assists {self.assists}"
+        return f"{self.name:20} {self.team}  {self.goals} + {self.assists} = {self.points}"
+
